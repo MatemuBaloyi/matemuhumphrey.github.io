87670b8253fa3c08b4eedbc441c0e4b503cf5e2e
21005059 <nduvhoace@gmail.com>
2024-07-21 13:47:01 +0200

    m

---
+++ .netlify/functions/emails.php
@@ -0,0 +1,57 @@
+<?php
+// Enable CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+// Ensure that the request method is OPTIONS for preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+
+// Logging input data for debugging
+error_log('Received input: ' . file_get_contents('php://input'));
+
+require '../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try {
+    // Parse JSON input from frontend
+    $input = json_decode(file_get_contents('php://input'), true);
+
+    // Logging parsed input for debugging
+    error_log('Parsed input: ' . print_r($input, true));
+
+    $name = htmlspecialchars($input['name']);
+    $email = htmlspecialchars($input['email']);
+    $message = htmlspecialchars($input['message']);
+
+    // Configure PHPMailer
+    $mail->isSMTP();
+    $mail->Host = getenv('SMTP_HOST');
+    $mail->SMTPAuth = true;
+    $mail->Username = getenv('SMTP_USERNAME');
+    $mail->Password = getenv('SMTP_PASSWORD');
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->setFrom(getenv('SMTP_USERNAME'), 'Matemu');
+    $mail->addAddress(getenv('SMTP_USERNAME'));
+
+    $mail->isHTML(true);
+    $mail->Subject = 'New Contact Form Submission';
+    $mail->Body    = "Name: $name<br>Email: $email<br>Message: $message";
+
+    // Send email
+    $mail->send();
+    echo json_encode(['message' => 'Message has been sent']);
+} catch (Exception $e) {
+    echo json_encode(['message' => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
+}
